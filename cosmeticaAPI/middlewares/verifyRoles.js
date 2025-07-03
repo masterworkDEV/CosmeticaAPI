3b891f38d055fa67a9d3f541a14ad780a6f9a503
masterworkDEV <godwinkelvin92@yahoo.com>
2025-07-03 05:14:52 +0100

    update

--- cosmeticaAPI/middlewares/verifyRoles.js
+++ cosmeticaAPI/middlewares/verifyRoles.js
@@ -1,14 +1,23 @@
 const verifyUserRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    if (!req.user || !req.user.roles) {
+    let userRoles = [];
+    if (req.user && Array.isArray(req.user.roles)) {
+      userRoles = req.user.roles;
+    } else if (Array.isArray(req.roles)) {
+      userRoles = req.roles;
+    }
+
+    if (userRoles.length === 0) {
       return res.status(403).json({
         message: "Access Denied: User roles not found or not authenticated.",
       });
     }
-    const { roles } = req.user;
 
     const availableRoles = [...allowedRoles];
-    const hasPermission = roles.some((role) => availableRoles.includes(role));
+    const hasPermission = userRoles.some((role) =>
+      availableRoles.includes(role)
+    );
+
     if (!hasPermission) {
       return res.status(403).json({
         message: "Access Denied: You do not have the required permissions.",
